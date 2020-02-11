5b9fb6f3cf70410b3f798247043b7eb589501ac8
Mike England <mike.england@webdevstudios.com>
2020-02-11 14:11:03 -0500

    Setup conditional file imports
    Only enqueue files that have been located to prevent accidental 404s.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -8,21 +8,29 @@ defined( 'ABSPATH' ) || exit;
  * @author Mike England <mike.england@webdevstudios.com>
  */
 function ded_blocks_editor_scripts() {
-	// Enqueue editor JS.
-	wp_enqueue_script(
-		'ded-blocks-js',
-		plugins_url( '../build/index.js', __FILE__ ),
-		[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
-		filemtime( plugin_dir_path( __FILE__ ) . $blockPath )
-	);
 
-	// Enqueue editor styles.
-	wp_enqueue_style(
-		'ded-editor-styles',
-		plugins_url( '../build/editor.css', __FILE__ ),
-		array( 'wp-edit-blocks' ),
-		filemtime( plugin_dir_path( __FILE__ ) . '../build/editor.css' )
-	);
+	$block_handler = '../build/index.js';
+	$editor_style_handler = '../build/style.css';
+
+	if ( file_exists ( plugin_dir_path( __FILE__ ) . $blockPath ) ) {
+		// Enqueue editor JS.
+		wp_enqueue_script(
+			'ded-blocks-js',
+			plugins_url( $block_handler , __FILE__ ),
+			[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
+			filemtime( plugin_dir_path( __FILE__ ) . $blockPath )
+		);
+	}
+
+	if ( file_exists ( plugin_dir_path( __FILE__ ) . $editor_style_handler ) ) {
+		// Enqueue editor styles.
+		wp_enqueue_style(
+			'ded-editor-styles',
+			plugins_url( $editor_style_handler, __FILE__ ),
+			array( 'wp-edit-blocks' ),
+			filemtime( plugin_dir_path( __FILE__ ) . $editor_style_handler )
+		);
+	}
 }
 add_action( 'enqueue_block_editor_assets', 'ded_blocks_editor_scripts' );
 
@@ -32,12 +40,20 @@ add_action( 'enqueue_block_editor_assets', 'ded_blocks_editor_scripts' );
  * @author Mike England <mike.england@webdevstudios.com>
  */
 function ded_register_block_styles() {
+
+	$style_handler = '../build/style.css';
+
+	// Bail if file not found.
+	if ( ! file_exists ( plugin_dir_path( __FILE__ ) . $style_handler ) ) {
+		return;
+	}
+
 	// Enqueue frontend styles.
 	wp_enqueue_style(
 		'ded-styles',
-		plugins_url( '../build/style.css', __FILE__ ),
+		plugins_url( $style_handler, __FILE__ ),
 		array( ),
-		filemtime( plugin_dir_path( __FILE__ ) . '../build/style.css' )
+		filemtime( plugin_dir_path( __FILE__ ) . $style_handler )
 	);
 }
 add_action( 'init', 'ded_register_block_styles' );
