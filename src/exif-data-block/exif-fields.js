719e2ca803b8bc68d41e2a214142cd4dd1f2ed89
Mike England <mike.england@webdevstudios.com>
2020-02-13 23:20:55 -0500

    Update ExifFields icon display
    Add material icons to list output.

--- src/exif-data-block/exif-fields.js
+++ src/exif-data-block/exif-fields.js
@@ -1,10 +1,15 @@
-const ExifFields = ( { exifData, allowedKeys } ) => {
+import ExifIcon from './material-icons';
 
+const ExifFields = ( { exifData, allowedKeys } ) => {
 	return (
 		<ul>
 			{ allowedKeys.map( ( key ) => {
+				console.log( JSON.stringify( key ) );
 				return (
-					<li key={ key }>{ `${ key }: ${ exifData[key].toString() }` }</li>
+					<li key={ key }>
+						<ExifIcon icon={ key } />
+						{ `${ key }: ${ exifData[key].toString() }` }
+					</li>
 				);
 			} ) }
 		</ul>
