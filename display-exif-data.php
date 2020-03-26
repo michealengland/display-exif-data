6e72830eea22f1c696e5156a39d5237275657993
Mike England <mike.england@webdevstudios.com>
2020-03-26 17:43:35 -0400

    Update to use WDS Block Starter
    - Add linter files
    - Add ignore files
    - Update .gitignore
    - Remove css.js since this file is no longer requireindex
    - Remove enueue-scripts.php since assets are now manage in main plugin file
    - Update webpack.config
    - Add PostCSS config
    - Update source file extension from css to scss
    - Update namespace and enqueues in main plugin file
    
    See https://github.com/WebDevStudios/wds-block-starter

--- display-exif-data.php
+++ display-exif-data.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Display Exif Data
  * Plugin URI:        https://github.com/michealengland/display-exif-data
  * Description:       Display exif data hidden in images.
- * Version:           0.0.3
+ * Version:           0.0.4
  * Author:            Mike England
  * Author URI:        https://twitter.com/mikelikethebike
  * License:           GPLv3
@@ -12,10 +12,83 @@
  * Tested up to:      5.3.0
  * Text Domain:       ded
  * Domain Path:       /languages
+ *
+ * @package WebDevStudios\DisplayExifData
+ * @since 0.0.4
  */
+
+namespace WebDevStudios\DisplayExifData;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Enqueue files.
-include_once( plugin_dir_path( __FILE__ ) . '/inc/enqueue-scripts.php' );
+/**
+ * Register the block with WordPress.
+ *
+ * @author WebDevStudios
+ * @since 0.0.1
+ */
+function register_block() {
+
+	// Define our assets.
+	$editor_script   = 'build/index.js';
+	$editor_style    = 'build/editor.css';
+	$frontend_style  = 'build/style.css';
+	$frontend_script = 'build/frontend.js';
+
+	// Verify we have an editor script.
+	if ( ! file_exists( plugin_dir_path( __FILE__ ) . $editor_script ) ) {
+		wp_die( esc_html__( 'Whoops! You need to run `npm run build` for the WDS Block Starter first.', 'ded' ) );
+	}
+
+	// Autoload dependencies and version.
+	$asset_file = require plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+
+	// Register editor script.
+	wp_register_script(
+		'ded-editor-script',
+		plugins_url( $editor_script, __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version'],
+		true
+	);
+
+	// Register editor style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $editor_style ) ) {
+		wp_register_style(
+			'ded-editor-style',
+			plugins_url( $editor_style, __FILE__ ),
+			[ 'wp-edit-blocks' ],
+			filemtime( plugin_dir_path( __FILE__ ) . $editor_style )
+		);
+	}
+
+	// Register frontend style.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_style ) ) {
+		wp_register_style(
+			'ded-style',
+			plugins_url( $frontend_style, __FILE__ ),
+			[],
+			filemtime( plugin_dir_path( __FILE__ ) . $frontend_style )
+		);
+	}
+
+	// Register block with WordPress.
+	register_block_type( 'ded/exif-data', array(
+		'editor_script' => 'ded-editor-script',
+		'editor_style'  => 'ded-editor-style',
+		'style'         => 'ded-style',
+	) );
+
+	// Register frontend script.
+	if ( file_exists( plugin_dir_path( __FILE__ ) . $frontend_script ) ) {
+		wp_enqueue_script(
+			'ded-frontend-script',
+			plugins_url( $frontend_script, __FILE__ ),
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true
+		);
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\register_block' );
