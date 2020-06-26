c98e702c9986d79a5b7c68ce23827ad1f6ebb4ae
Mike England <mike.england@webdevstudios.com>
2020-06-26 19:18:10 -0400

    Update exif fields display
    - Import helper functions
    - Use formatFields to cleanup how fields display on the front-end
    - Use convertKeysToIcons to change key values to icon friendly values
    - Add lodash features to improve display case on the front-end

--- src/exif-data-block/exif-fields.js
+++ src/exif-data-block/exif-fields.js
@@ -3,23 +3,27 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-
 import ExifIcon from './material-icons';
+import { convertKeysToIcons, formatFields } from '../utils';
+import {startCase, camelCase} from 'lodash';
 
 // Import plugin settings.
 const {
-	options
+	options,
 } = dedOptions;
 
 const ExifFields = ( { allowedKeys, displayIcon, exifData } ) => {
 	// Remove disabled fields in plugin settings fomr allowedKeys.
 	const enabledFields = allowedKeys.filter(x => ! Object.keys( options ).includes( x ) );
 
+	// Format object fields for front-end.
+	const formattedFields = formatFields(exifData);
+
 	// List of fields.
 	const listItems = enabledFields.map( ( key ) => (
 		<li key={ key }>
-			{ displayIcon && <ExifIcon icon={ key } /> }
-			{ `${ key }: ${ exifData[key].toString() }` }
+			{ displayIcon && <ExifIcon icon={ convertKeysToIcons(key, exifData) } /> }
+			{ `${ startCase( camelCase( key.replace(/_/g, ' ' ) ) ) }: ${ formattedFields[key].toString() }` }
 		</li>
 	) );
 
