dca68c3a4012c820db086f4bceb401f3fc249cbc
Mike England <mike.england@webdevstudios.com>
2020-06-26 19:25:29 -0400

    Create index for imports
    Allows for importing of all utility functions

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export * from './format-fields.js';
+export * from './format-unix-timestamp.js';
+export * from './icon-keys.js';
+export * from './icon-keys.js';
+export * from './remove-empty-properties.js';
